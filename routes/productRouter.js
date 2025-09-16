3baff3dd10dcd0e9923b0cd06406132c39c37ecc
Ihsaan7 <ihsaan2215@gmail.com>
2025-09-16 19:31:26 +0500

    Admin/Product_Basic_Setup

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const debug = require("debug")("mini-ecommerce:product")
 const router = express.Router()
+const upload = require("../config/multerConfig")
 
-router.get("/",(req,res)=>
+// router.get("/",(req,res)=>
+//     {
+//         debug("Product route accessed")
+//         res.send("hey its Working - Product Route")
+//     })
+router.post("/create", upload.single("image") ,(req,res)=>
     {
-        debug("Product route accessed")
-        res.send("hey its Working - Product Route")
+       res.send(req.file)
     })
 
 module.exports = router
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ const debug = require("debug")("mini-ecommerce:user")
 const router = express.Router()
 const {registerUser} = require("../controllers/authController")
 const {loginUser} = require("../controllers/authController")
+const isLoggedIn = require("../middelwares/isLoggedIn")
 
 router.get("/",(req,res)=>
     {
@@ -15,8 +16,14 @@ router.get("/login",(req,res)=>{
 })
 router.post("/login" , loginUser )
 
-router.get("/shop",(req,res)=>
+router.get("/shop",isLoggedIn,(req,res)=>
     {
         res.render("shop")
     })
+router.get("/logout",(req,res)=>
+    {
+        res.cookie("token", "")
+        res.redirect("/")
+    })
+    
 module.exports = router
