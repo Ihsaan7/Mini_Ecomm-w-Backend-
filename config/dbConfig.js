7bcd032daa1e77eea2dfcf8a1d7dc1af70553596
Ihsaan7 <ihsaan2215@gmail.com>
2025-09-17 15:48:28 +0500

    Update database config for Vercel deployment

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,10 +3,15 @@ const config = require("config")
 const debug = require("debug")("mini-ecommerce:db")
 require('dotenv').config();
 
+// Use Vercel-specific MongoDB URI in production, fallback to regular URI
+const mongoUri = process.env.NODE_ENV === 'production' && process.env.MONGODB_URI_VERCEL 
+  ? process.env.MONGODB_URI_VERCEL 
+  : process.env.MONGODB_URI;
+
 mongoose
-.connect(process.env.MONGODB_URI)
-.then(()=>{debug("Connected")})
-.catch((err)=>{debug(err)})
+.connect(mongoUri)
+.then(()=>{debug("Connected to MongoDB")})
+.catch((err)=>{debug("MongoDB connection error:", err)})
 
 
 module.exports = mongoose.connection
