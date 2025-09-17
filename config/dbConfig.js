c50db66bc06a6ca74c6247925c3717b06eb7db92
Ihsaan7 <ihsaan2215@gmail.com>
2025-09-17 15:56:31 +0500

    Improve MongoDB connection for Vercel with better timeout settings

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -8,8 +8,20 @@ const mongoUri = process.env.NODE_ENV === 'production' && process.env.MONGODB_UR
   ? process.env.MONGODB_URI_VERCEL 
   : process.env.MONGODB_URI;
 
+// Connection options for better reliability in serverless environment
+const connectionOptions = {
+  serverSelectionTimeoutMS: 30000, // 30 seconds
+  socketTimeoutMS: 45000, // 45 seconds
+  bufferMaxEntries: 0, // Disable mongoose buffering
+  bufferCommands: false, // Disable mongoose buffering
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  minPoolSize: 5, // Maintain a minimum of 5 socket connections
+  maxIdleTimeMS: 30000, // Close connections after 30 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+};
+
 mongoose
-.connect(mongoUri)
+.connect(mongoUri, connectionOptions)
 .then(()=>{debug("Connected to MongoDB")})
 .catch((err)=>{debug("MongoDB connection error:", err)})
 
